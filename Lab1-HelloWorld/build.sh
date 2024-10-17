48573357e8ef6b3c95276891c8f7afb76c480d5f
fs.anvr <fs.anvr@gmail.com>
2024-10-17 14:10:06 +1000

    Linux scripts

---
+++ Lab1-HelloWorld/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# === Конфигурация ===
+BUILD_DIR=build
+REPO_URL=https://github.com/fsanvr/OS.git
+CMAKE_GENERATOR="MinGW Makefiles"
+CMAKE_COMPILER=g++
+
+# === Обновление исходных кодов ===
+echo "Updating source code from Git repository..."
+git pull %REPO_URL% 
+
+# === Создание build директории ===
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir -p "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR" || exit
+
+# === Настройка ===
+echo "Configuring project with CMake..."
+cmake -G "$CMAKE_GENERATOR"
+if [ $? -ne 0 ]; then
+    echo "CMake configuration failed!"
+    exit 1
+fi
+
+# === Компиляция ===
+echo "Building project..."
+mingw32-make
+if [ $? -ne 0 ]; then
+    echo "Build failed!"
+    exit 1
+fi
+
+# === Завершение сборки ===
+echo "Build completed successfully!"
+cd ..
+exit 0
