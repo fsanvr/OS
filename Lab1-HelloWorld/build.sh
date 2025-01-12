2010406c34154a38e6c963f51d89885d415aff74
fs.anvr <fs.anvr@gmail.com>
2025-01-12 16:06:20 +1000

    simplification of update and build code for lab1

--- Lab1-HelloWorld/build.sh
+++ Lab1-HelloWorld/build.sh
@@ -1,39 +1,10 @@
-#!/bin/bash
+git pull origin main
 
-# === Конфигурация ===
-BUILD_DIR="build"
-REPO_URL="https://github.com/fsanvr/OS.git"
-CMAKE_GENERATOR="Unix Makefiles"
-CMAKE_COMPILER="g++"
+mkdir build
+cd build
 
-# === Обновление исходных кодов ===
-echo "Updating source code from Git repository..."
-git pull %REPO_URL% 
+cmake ../
 
-# === Создание build директории ===
-if [ ! -d "$BUILD_DIR" ]; then
-    mkdir -p "$BUILD_DIR"
-fi
-
-cd "$BUILD_DIR" || exit
-
-# === Настройка ===
-echo "Configuring project with CMake..."
-cmake -G "$CMAKE_GENERATOR"
-if [ $? -ne 0 ]; then
-    echo "CMake configuration failed!"
-    exit 1
-fi
-
-# === Компиляция ===
-echo "Building project..."
 cmake --build .
-if [ $? -ne 0 ]; then
-    echo "Build failed!"
-    exit 1
-fi
 
-# === Завершение сборки ===
-echo "Build completed successfully!"
-cd ..
-exit 0
+./HelloWorld
