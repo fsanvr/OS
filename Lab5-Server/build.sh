7e377b6745a93e878e6cff530da795e7a6726f8c
fs.anvr <fs.anvr@gmail.com>
2025-01-17 01:56:25 +1000

    update build scripts for autostart in lab5 and lab7

--- Lab5-Server/build.sh
+++ Lab5-Server/build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 git pull origin main
 
-lxterminal -e "bash -c 'socat -d -d pty,raw,echo=0 pty,raw,echo=0; exec bash'"
+qterminal -e "bash -c 'socat -d -d pty,raw,echo=0 pty,raw,echo=0; exec bash'" &
 
 mkdir build
 cd build
